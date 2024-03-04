784431349638692e5e117a06daf735891091f366
9mlih <abdalkawi2001@gmail.com>
2024-03-05 01:20:09 +0300

    card

---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Card = () => {
+    return (
+         <div className="contener">
+          <img className="image" src="logo192.png"  alt="" />
+          <p>abdalkwi</p>
+         </div>
+     )
+};
+
+export default Card;
